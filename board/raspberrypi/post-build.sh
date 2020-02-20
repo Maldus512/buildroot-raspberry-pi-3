cd38333d33a37bb451ffc195e13b726974ab33f6
rock@aang <mattia512maldini@gmail.com>
2020-02-20 14:21:28 +0100

    Removed boot logs and added serial console on ttyUSB0; TODO: check if everything works on rebuild

--- board/raspberrypi/post-build.sh
+++ board/raspberrypi/post-build.sh
@@ -4,11 +4,11 @@ set -u
 set -e
 
 # Add a console on tty1
-if [ -e ${TARGET_DIR}/etc/inittab ]; then
-    grep -qE '^tty1::' ${TARGET_DIR}/etc/inittab || \
-	sed -i '/GENERIC_SERIAL/a\
-tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
-fi
+#if [ -e ${TARGET_DIR}/etc/inittab ]; then
+#    grep -qE '^tty1::' ${TARGET_DIR}/etc/inittab || \
+#	sed -i '/GENERIC_SERIAL/a\
+#tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
+#fi
 
 cp package/busybox/S10mdev ${TARGET_DIR}/etc/init.d/S10mdev
 chmod 755 ${TARGET_DIR}/etc/init.d/S10mdev
